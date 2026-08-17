7e6c60a3fd7e2bac38348ad1e8c0105c1a0aaef9
fedes1to <fedesito@posteo.es>
2026-08-17 19:03:14 +0200

    better save json file

--- internal/config/io.go
+++ internal/config/io.go
@@ -3,35 +3,17 @@ package config
 import (
 	"encoding/json"
 	"errors"
-	"fmt"
-	"io"
 	"os"
 )
 
 func SafeSaveJsonFile(path string, from any) error {
-	backupPath := path + ".old"
-	oldJson, openErr := os.Open(path)
-	backupJson, backupErr := os.Create(backupPath)
-
-	if openErr != nil && !errors.Is(openErr, os.ErrNotExist) {
-		fmt.Fprintln(os.Stderr, "Couldn't open old JSON for backup,", openErr)
-	} else if backupErr != nil {
-		fmt.Fprintln(os.Stderr, "Couldn't access target backup JSON,", backupErr)
-	} else if openErr == nil {
-		_, copyErr := io.Copy(backupJson, oldJson)
-		if copyErr != nil {
-			fmt.Fprintln(os.Stderr, "Couldn't copy backup JSON,", copyErr)
-		}
-		defer oldJson.Close()
-		defer backupJson.Close()
-	}
-
-	// truncates if it exists, thats why we made a .old
-	jsonFile, err := os.Create(path)
+	tmpPath := path + ".tmp"
+	jsonFile, err := os.Create(tmpPath)
 	if err != nil {
 		return err
 	}
 	defer jsonFile.Close()
+	defer os.Remove(tmpPath)
 
 	encoder := json.NewEncoder(jsonFile)
 	encoder.SetIndent("", "  ") // make it tolerable to see
@@ -39,7 +21,12 @@ func SafeSaveJsonFile(path string, from any) error {
 		return err
 	}
 
-	return nil
+	if err := jsonFile.Sync(); err != nil {
+		return err
+	}
+
+	// either the new file or the old
+	return os.Rename(tmpPath, path)
 }
 
 func LoadJsonFile[T any](path string, into *T) error {
