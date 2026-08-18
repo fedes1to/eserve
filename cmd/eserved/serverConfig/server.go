4b4fcd4a45c5169f32bf5eeddebabfcf06a1cce9
fedes1to <fedesito@posteo.es>
2026-08-18 12:36:10 +0200

    make serversettings not as horrible

--- cmd/eserved/serverConfig/server.go
+++ cmd/eserved/serverConfig/server.go
@@ -11,12 +11,14 @@ import (
 
 type ServerSettings struct {
 	ListenAddr     string `json:"listen_addr"`
-	BuildThreads   int    `json:"build_threads"`
-	PerUserThreads int    `json:"per_user_threads"`
+	BuildThreads   int    `json:"build_threads"`    // 0 = unlimited
+	PerUserThreads int    `json:"per_user_threads"` // 0 = unlimited
 	ChrootBase     string `json:"chroot_base"`
-	StagePath      string `json:"stage_base"`
+	StagePath      string `json:"stage_path"`
 	TlsCertPath    string `json:"tls_cert_path"`
 	TlsKeyPath     string `json:"tls_key_path"`
+	BaseBinhostURL string `json:"base_binhost_url"`
+	RepoBase       string `json:"repo_base"`
 }
 
 var (
@@ -31,20 +33,24 @@ func LoadServerSettings() error {
 }
 
 func InitializeServerSettings() error {
-	if _, err := os.Stat(serverSettingsPath); err != nil {
-		if errors.Is(err, os.ErrNotExist) {
-			log.Println("Generating default server config")
-			Settings.BuildThreads = 0
-			Settings.PerUserThreads = 0
-			Settings.ChrootBase = "/srv/build"
-			Settings.ListenAddr = "127.0.0.1:8080"
-			Settings.TlsCertPath = filepath.Join(ServerConfigPath, "server.crt")
-			Settings.TlsKeyPath = filepath.Join(ServerConfigPath, "server.key")
-			Settings.StagePath = filepath.Join(ServerConfigPath, "stages")
-			return config.SafeSaveJsonFile(serverSettingsPath, Settings)
-		}
+	_, err := os.Stat(serverSettingsPath)
+	if err == nil {
+		return LoadServerSettings()
+	}
+	if !errors.Is(err, os.ErrNotExist) {
 		return err
 	}
 
-	return LoadServerSettings()
+	log.Println("Generating default server config")
+	const listenAddr = "127.0.0.1:8080"
+	Settings = ServerSettings{
+		ListenAddr:     listenAddr,
+		ChrootBase:     "/srv/build",
+		RepoBase:       "/srv/pkgs",
+		BaseBinhostURL: "https://" + listenAddr + "/pkgs",
+		StagePath:      filepath.Join(ServerConfigPath, "stages"),
+		TlsCertPath:    filepath.Join(ServerConfigPath, "server.crt"),
+		TlsKeyPath:     filepath.Join(ServerConfigPath, "server.key"),
+	}
+	return config.SafeSaveJsonFile(serverSettingsPath, Settings)
 }
